0e2096d9077c6319c807ddd696f63bad4ebc9264
ZAROUR Mahdi <zaarourmehdi@yahoo.fr>
2021-01-03 19:08:53 -0500

    delete unecessary files
    delete things_to_put_in_ipynb_file

--- things_to_put_in_the_ypnb_file.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import solution
-import matplotlib.pyplot as plt
-
-data = np.genfromtxt('data_banknote_authentication.txt', delimiter=',')
-train_inputs = data[:,:-1]
-train_labels = data[:,-1]
-
-solution.get_test_errors_projections(data, with_plot=True, number_of_projections=2, with_e_bars=True)
